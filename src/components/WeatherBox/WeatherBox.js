c881a3c1857d9a78bdd18cd484ae4b3e6f76482a
MaciejMl <szypki@gmail.com>
2023-04-06 21:09:01 +0200

    Add ErrorBox to WeatherBox and use error state when no city

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,36 +2,44 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const WeatherBox = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [pending, setPending] = useState(false);
+  const [error, setError] = useState(false);
   const handleCityChange = useCallback((cityName) => {
     console.log(cityName);
 
     setPending(true);
+    setError(false);
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=cb98283567cbd64074522f5db207fdf4&units=metric`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setPending(false);
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
-        setWeatherData(weatherData);
-      });
+    ).then((res) => {
+      if (res.status === 200) {
+        return res.json().then((data) => {
+          console.log(data);
+          const weatherData = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
+          setWeatherData(weatherData);
+          setPending(false);
+        });
+      } else {
+        setError(true);
+      }
+    });
   }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      {pending && !weatherData && <Loader />}
-      {weatherData && <WeatherSummary {...weatherData} />}
+      {pending && !error && <Loader />}
+      {error && <ErrorBox />}
+      {weatherData && !pending && !error && <WeatherSummary {...weatherData} />}
     </section>
   );
 };
